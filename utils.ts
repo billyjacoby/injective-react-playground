0217dc6c17ab21191e7f4bac0c85236535492855
billyjacoby <billyjacoby@gmail.com>
2024-12-13 10:41:56 -0500

    fix: pin hono version

--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-import { hc } from 'hono/client';
 import type { AppType } from 'helix-mobile-server';
+import { hc } from 'hono/client';
 
 export const client = hc<AppType>('http://10.0.1.60:3000');
